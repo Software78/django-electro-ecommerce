3a5d4fd56b46b3c46efcad35d1bf8a842a0648b3
abaru adedola <technicatalyst78@gmail.com>
2021-06-02 19:54:42 +0000

    main

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('checkout/',checkout,name='checkout'),
+    path('product/',product,name='product'),
+    path('blank/',blank,name='blank'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request,'index.html',context)
+
+def checkout(request):
+    context = {}
+    return render(request,'checkout.html',context)
+
+def product(request):
+    context ={}
+    return render(request,'product.html')
+
+def blank(request):
+    context = {}
+    return render(request,'blank.html')
